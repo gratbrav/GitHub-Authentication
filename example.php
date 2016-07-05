e7646bfd901c93ef0e7c35472ef1245227e3b18f
Gratbrav <spam@huxsohl.de>
2016-07-05 21:10:13 +0200

    add an example

---
+++ example.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     * place this script in your github redirect file
+     */
+
+    require_once 'Class/autoload.php';
+
+    $config = array(
+        'client_id' => 'github client id',
+        'client_secret' => 'github client secret',
+        'redirect_url' => 'github redirect url', 
+        'app_name' => 'github app name'
+    );
+
+    $login = new Github_Login($config);
+    $data = $login->authenticate($_GET);
+
+    echo '<pre>';
+    print_r($data);
